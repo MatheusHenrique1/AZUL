a1e6bde5fee456b7e121d4ae8d4764767e08f548
MatheusHenrique1 <121906091+MatheusHenrique1@users.noreply.github.com>
2026-04-06 18:39:37 -0300

    a6.py

--- a6.py
+++ a6.py
@@ -4,6 +4,6 @@ print("O tipo do valor é: ",type(n1))
 
 if n1.isdigit():
     numero = int(n1)
-    print("O quadrado é: ", numero * numero)
+    print("O quadrado é: ", numero ** 2)
 else: 
     print("Não é número")
