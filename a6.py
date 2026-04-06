0d2994194d9f317a254e5e3832ed7cb80c3eaa7c
MatheusHenrique1 <luizfragoso583@gmail.com>
2026-04-06 15:03:12 -0300

    Create a6.py

---
+++ a6.py
@@ -0,0 +1,9 @@
+#Leia um valor e: Mostre o tipo; Se for numérico (após conversão) → mostre o quadrado
+n1 = (input("Digite o valor: "))
+print("O tipo do valor é: ",type(n1))
+
+if n1.isdigit():
+    numero = int(n1)
+    print("O quadrado é: ", numero * numero)
+else: 
+    print("Não é número")
