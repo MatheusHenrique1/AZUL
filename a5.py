e2a676370ccbe489e6e175c9ef5f80cf5f009ff5
MatheusHenrique1 <luizfragoso583@gmail.com>
2026-04-06 14:55:49 -0300

    Create a5.py

---
+++ a5.py
@@ -0,0 +1,6 @@
+#Leia um número e: Se estiver entre 10 e 20 → “Dentro”; Caso contrário → “Fora”
+n1 = int(input("Digite o seu valor: "))
+if (n1 >= 10) and (n1 <= 20):
+    print("Dentro!!")
+else: 
+    print("Fora.")
